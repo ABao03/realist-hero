47644d8676bcea42df0979debe1ea81a2587b508
M0hamedd <97306128+M0hamedd@users.noreply.github.com>
2023-10-24 20:51:08 -0400

    added working menu

--- Menu.gd
+++ Menu.gd
@@ -3,8 +3,15 @@ func _on_start_game_pressed():
 	get_tree().change_scene_to_file("res://game.tscn")
 
 
+func _on_settings_pressed():
+	get_tree().change_scene_to_file("res://Settings.tscn")
+
+
 func _on_quit_pressed():
 	get_tree().quit()
 	
+
+
+
 
 
