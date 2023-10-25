79daf857fc96e0809e71665cf252e9e9e9b32029
M0hamedd <97306128+M0hamedd@users.noreply.github.com>
2023-10-24 21:08:35 -0400

    test

--- Settings.gd
+++ Settings.gd
@@ -1,4 +1,7 @@
 extends Control
 
+
+
+
 func _on_go_back_pressed():
-	get_tree().change_scene_to_file("res://menu.tscn")
+	get_tree().change_scene_to_file("res://menu.tscn") # Replace with function body.
