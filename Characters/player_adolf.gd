5703b2eb5b4ac1e20b7a4c2eb0b157a7a9a3c3dd
ABao03 <87954502+ABao03@users.noreply.github.com>
2024-03-30 10:24:40 -0400

    fixed mohamed's garbage code

--- Characters/player_adolf.gd
+++ Characters/player_adolf.gd
@@ -1,8 +1,11 @@
+# https://www.youtube.com/watch?v=Xf2RduncoNU
+
 extends CharacterBody2D
 
 @export var speed : float = 100
 @export var starting : Vector2 = Vector2(0, 1)
 @onready var animation = $AnimationPlayer/AnimationTree
+@onready var sprite = $Sprite2D
 @onready var state_machine = animation.get('parameters/playback')
 func _ready():
 	update_animation(starting)
@@ -12,14 +15,26 @@ func _physics_process(_delta):
 		Input.get_action_strength('right') - Input.get_action_strength('left'),
 		Input.get_action_strength('down') - Input.get_action_strength('up')
 	)
-	update_animation(input_direction)
+	
+	velocity = velocity.normalized()
 	velocity = input_direction * speed
+		
 	move_and_slide()
 	new_state()
+	
+	update_animation(input_direction)
+	
 func update_animation(move_input: Vector2):
-	if move_input != Vector2.ZERO:
-		animation.set('parameters/Walk/blend_position', move_input)
-		animation.set("parameters/Walk/blend_position", move_input)
+	if move_input == Vector2.ZERO:
+		animation["parameters/conditions/idle"] = true
+		animation["parameters/conditions/walk"] = false
+	else:
+		animation["parameters/conditions/idle"] = false
+		animation["parameters/conditions/walk"] = true
+		
+		animation["parameters/Idle/blend_position"] = move_input
+		animation["parameters/Walk/blend_position"] = move_input
+		
 func new_state():
 	if velocity != Vector2.ZERO:
 		state_machine.travel('Walk')
