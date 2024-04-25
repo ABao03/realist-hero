5364fa7b3e9f92a9d1247c1260529c5e212f7c79
LeoXing2003 <58889111+LeoXing2003@users.noreply.github.com>
2024-04-25 00:20:56 -0400

    Added pause menu
    Added a pause menu
    Note: When returning to main menu through the pause menu, the buttons on the main menu will not respond

--- Menu/Menu.gd
+++ Menu/Menu.gd
@@ -1,6 +1,6 @@
 extends Control
 func _on_start_game_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://gamemanager.tscn")
 
 
 func _on_settings_pressed():
