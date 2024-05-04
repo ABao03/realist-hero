ff047ec334deb6d0062569905c5ee15fd21b81c4
LeoXing2003 <58889111+LeoXing2003@users.noreply.github.com>
2024-05-03 21:15:27 -0400

    Revert "new enemy type (again)"
    This reverts commit dae484247769c1a1da636569e07a38a83367777c, reversing
    changes made to 05950f8a61684761e669ee3d1ead9a0514c9a6c1.

--- Characters/item_option.gd
+++ Characters/item_option.gd
@@ -14,9 +14,10 @@ func _ready():
 	connect("selected_upgrade",Callable(player,"upgrade_character"))
 	#if item == null:
 		#item = "food"
-	lblName.text = item["Name"]
-	lblDescription.text = item["Info"]
-	itemIcon.texture = load("res://Assets/" + item["Name"] + ".png")
+	#lblName.text = UpgradeDb.UPGRADES[item]["displayname"]
+	#lblDescription.text = UpgradeDb.UPGRADES[item]["details"]
+	#lblLevel.text = UpgradeDb.UPGRADES[item]["level"]
+	#itemIcon.texture = load(UpgradeDb.UPGRADES[item]["icon"])
 	
 func _input(event):
 	if event.is_action("mouse_leftclick"):
@@ -24,6 +25,7 @@ func _input(event):
 			emit_signal("selected_upgrade",item)
 
 func _on_mouse_entered():
+	print("ok")
 	mouse_over = true
 
 func _on_mouse_exited():
--- gamemanager.gd
+++ gamemanager.gd
@@ -1,9 +1,5 @@
-extends Node2D
+extends Node
 
-#UI nodes
-signal inventory_open()
-signal inventory_closed()
-var inventoryOpen = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,9 +8,4 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("i") && inventoryOpen == true:
-		inventoryOpen = false
-		emit_signal("inventory_closed")
-	elif Input.is_action_just_pressed("i"):
-		inventoryOpen = true
-		emit_signal("inventory_open")
+	pass
