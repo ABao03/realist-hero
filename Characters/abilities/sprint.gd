5a2c99911ab84d4a8719d69c3f4f637d38b7da33
LeoXing2003 <58889111+LeoXing2003@users.noreply.github.com>
2024-05-08 23:25:10 -0400

    sprint fixed

--- Characters/abilities/sprint.gd
+++ Characters/abilities/sprint.gd
@@ -1,13 +1,14 @@
 extends Node2D
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var hurtbox = get_node("../HurtBox")
-var originalSpeed
-var sprintSpeed = 100
+@onready var originalSpeed
+@onready var sprintSpeed = 50
 var isReady : bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	originalSpeed = player.speed
+	print(hurtbox)
 
 func _input(event: InputEvent): # Show/hide pause menu
 	if event.is_action_pressed("shift") and isReady == true:
@@ -17,12 +18,13 @@ func _input(event: InputEvent): # Show/hide pause menu
 		isReady = false
 		hurtbox.collision.call_deferred("set", "disabled", true)
 		player.collision.call_deferred("set", "disabled", true)
-		
+			
 	else:
 		pass
 
 func _on_sprint_button_pressed():
 	if isReady == true:
+
 		$Timer.start()
 		$CooldownTimer.start()
 		player.speed = sprintSpeed
