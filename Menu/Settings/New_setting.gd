cfd44df74e2461fa25c222f3c07c8855cf114b3d
LeoXing2003 <58889111+LeoXing2003@users.noreply.github.com>
2024-05-30 21:45:13 -0400

    added fullscreen functionality

--- Menu/Settings/New_setting.gd
+++ Menu/Settings/New_setting.gd
@@ -2,3 +2,10 @@ extends Control
 
 func _on_return_pressed():
 	get_tree().change_scene_to_file("res://Menu/menu.tscn")
+	
+
+func _on_windowed_pressed():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_fullscreen_pressed():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
