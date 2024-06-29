7d901d1c5f348549fa60e8e28e2d38380f222741
LeoXing2003 <58889111+LeoXing2003@users.noreply.github.com>
2024-06-28 22:38:06 -0400

    adjusted the boss enemy

--- Characters/Enemy/ranged_boss.gd
+++ Characters/Enemy/ranged_boss.gd
@@ -60,7 +60,7 @@ func _physics_process(_delta):
 			arrow_instance.global_position = $Marker2D.global_position
 			add_child(arrow_instance)
 				
-			await get_tree().create_timer(15).timeout # Delay between projectile atacks
+			await get_tree().create_timer(20).timeout # Delay between projectile atacks
 			bow_cooldown = true
 		
 	if hurtAnimationPlaying == true:
